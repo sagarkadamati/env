f8643bb45660127ed0f966b1f97956e8440ee789
sagar <sagar@kadamati.(none)>
2015-04-30 20:38:13 +0530

    Added initial files

---
+++ emacs.el
@@ -0,0 +1,20 @@
+;; -*- mode: emacs-lisp; -*-
+
+(load "~/env/elisp/core-utils.el")
+(add-to-load-path "~/env/elisp/")
+
+(load "misc-config.el")
+(load "c-mode-config.el")
+(load "diff-config.el")
+(load "recentf-config.el")
+(load "highlight-symbol.el")
+(load "vlf.el")
+
+(load "cscope-config.el")
+;; (load "chaf-config.el")
+
+(load "elscreen-config.el")
+(load "custom.el")
+(load "experimental.el")
+(load "p4.el")
+(load "package-install.el")
