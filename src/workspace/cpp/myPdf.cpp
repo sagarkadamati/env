5ff9d480b0559fe6e9ec57777b9383e5e6c77d85
Sagar Kadamati <skadamati@nvidia.com>
2015-06-04 19:31:38 +0530

    updated pdf

--- src/workspace/cpp/myPdf.cpp
+++ src/workspace/cpp/myPdf.cpp
@@ -6,25 +6,49 @@ using namespace PoDoFo;
 
 int main()
 {
-	PoDoFo::PdfMemDocument pdf("mysample.pdf");
 	int pn, pc;
+
+	PdfMemDocument pdf("mysample.pdf");
+	PdfStreamedDocument pdfout("output.pdf");
+	PdfPainter painter;
+	PdfPage* pPage;
+	PdfFont* pFont;
+
+	pdfout.GetInfo()->SetCreator ( PdfString("examplahelloworld - A PoDoFo test application") );
+	pdfout.GetInfo()->SetAuthor  ( PdfString("Dominik Seichter") );
+	pdfout.GetInfo()->SetTitle   ( PdfString("Hello World") );
+	pdfout.GetInfo()->SetSubject ( PdfString("Testing the PoDoFo PDF Library") );
+	pdfout.GetInfo()->SetKeywords( PdfString("Test;PDF;Hello World;") );
 
 	pc = pdf.GetPageCount();
 	cout << "Pages: " << pc << endl;
 
-	// for (pn = 0; pn < pc; ++pn) {
-	// 	PoDoFo::PdfPage* page = pdf.GetPage(pn);
+	for (pn = 0; pn < pc; ++pn) {
+		PdfPage* page = pdf.GetPage(pn);
+		PdfContentsTokenizer tok(page);
+		PdfVariant var;
+		EPdfContentsType type;
+		const char* token = NULL;
 
-	// 	PoDoFo::PdfContentsTokenizer tok(page);
-	// 	const char* token = nullptr;
-	// 	PoDoFo::PdfVariant var;
-	// 	PoDoFo::EPdfContentsType type;
-	// 	while (tok.ReadNext(type, token, var)) {
-	// 		if (type == PoDoFo::ePdfContentsType_Keyword) {
-	// 			// process type, token & var
-	// 		}
-	// 	}
-	// }
+		while (tok.ReadNext(type, token, var)) {
+			if (type == PoDoFo::ePdfContentsType_Keyword) {
+				// process type, token & var
+			}
+		}
+
+		pPage = pdfout.CreatePage(page->GetPageSize());
+
+		pFont = pdfout.CreateFont( "Arial" );
+		pFont->SetFontSize( 18.0 );
+
+		painter.SetPage( pPage );
+		painter.SetFont( pFont );
+		painter.DrawText( 56.69, pPage->GetPageSize().GetHeight() - 56.69, "Hello World!" );
+
+		painter.FinishPage();
+	}
+
+	pdfout.Close();
 
 	return 0;
 }
