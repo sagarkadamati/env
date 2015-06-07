5be5435aff54dff802e83ecafa2c34d7bc59320a
Sagar Kadamati <sagarkadamati@gmail.com>
2015-06-07 18:27:48 +0530

    Modified

--- src/workspace/cpp/myPdf.cpp
+++ src/workspace/cpp/myPdf.cpp
@@ -17,7 +17,7 @@ void ReplaceStringInPlace(std::string& subject, const std::string& search,
 	}
 }
 
-std::string extract( PdfMemDocument* pDocument, PdfPage* pPage ) 
+std::string extract( PdfMemDocument* pDocument, PdfPage* pPage , string removefont) 
 {
 	const char*      pszToken = NULL;
 	PdfVariant       var;
@@ -45,9 +45,13 @@ std::string extract( PdfMemDocument* pDocument, PdfPage* pPage )
 	double size;
 	double g;
 	string str;
-	
+
+	bool record = true;
+
 	bool   bTextBlock   = false;
 	PdfFont* pCurFont   = NULL;
+
+	int count = 0;
 
 	std::stack<PdfVariant> stack;
 	std::ostringstream block;
@@ -56,151 +60,8 @@ std::string extract( PdfMemDocument* pDocument, PdfPage* pPage )
 	{
 		if( eType == ePdfContentsType_Keyword )
 		{
-			if( strcmp( pszToken, "cm" ) == 0 ) 
-			{
-				offset_y = stack.top().GetReal();
-				stack.pop();
-				offset_x = stack.top().GetReal();
-				stack.pop();
-				scale_y  = stack.top().GetReal();
-				stack.pop();
-				shear_y  = stack.top().GetReal();
-				stack.pop();
-				shear_x  = stack.top().GetReal();
-				stack.pop();
-				scale_x  = stack.top().GetReal();
-				stack.pop();
-				
-				block << scale_x << " " 
-				      << shear_x << " " 
-				      << shear_y << " " 
-				      << scale_y << " " 
-				      << offset_x << " " 
-				      << offset_y << " cm " << endl;
-			}
-
-			if( strcmp( pszToken, "re" ) == 0 ) 
-			{
-				offset_y = stack.top().GetReal();
-				stack.pop();
-				offset_x = stack.top().GetReal();
-				stack.pop();
-				scale_y  = stack.top().GetReal();
-				stack.pop();
-				shear_y  = stack.top().GetReal();
-				stack.pop();
-				
-				block << shear_y << " " 
-				      << scale_y << " " 
-				      << offset_x << " " 
-				      << offset_y << " " 
-				      << pszToken << " " << endl;
-			}
-
-			if( strcmp( pszToken, "W" ) == 0 ||
-			    strcmp( pszToken, "n" ) == 0 ||
-			    strcmp( pszToken, "q" ) == 0 ||
-			    strcmp( pszToken, "Q" ) == 0 )
-			{
-				block << pszToken << endl;
-			}
-
-			if( strcmp( pszToken, "w" ) == 0 ||
-			    strcmp( pszToken, "Tz" ) == 0 ||
-			    strcmp( pszToken, "g" ) == 0 ||
-			    strcmp( pszToken, "G" ) == 0 )
-			{
-				g = stack.top().GetReal();
-				stack.pop();
-				block << g << " " << pszToken << endl;
-			}
-
-			if( strcmp( pszToken, "rg" ) == 0 ||
-			    strcmp( pszToken, "RG" ) == 0 )
-			{
-				blue = stack.top().GetReal();
-				stack.pop();
-				green = stack.top().GetReal();
-				stack.pop();
-				red = stack.top().GetReal();
-				stack.pop();
-
-				blue = 0.0;
-				green = 0.0;
-				red = 0.0;
-				
-				block << red << " " 
-				      << green << " " 
-				      << blue << " " 
-				      << pszToken << endl;
-
-				// cout << red << " " 
-				//       << green << " " 
-				//       << blue << " " 
-				//       << pszToken << endl;
-			}
-
-			// support 'l' and 'm' tokens
-			if( strcmp( pszToken, "l" ) == 0 || 
-			    strcmp( pszToken, "m" ) == 0 )
-			{
-				x = stack.top().GetReal();
-				stack.pop();
-				y = stack.top().GetReal();
-				stack.pop();
-			}
-			else if( strcmp( pszToken, "BT" ) == 0 ) 
-			{
-				bTextBlock   = true;     
-				block << "BT" << endl;
-			}
-			else if( strcmp( pszToken, "ET" ) == 0 ) 
-			{
-				block << "ET" << endl;
-			}
-
-			if( bTextBlock ) 
-			{
-				if( strcmp( pszToken, "gs" ) == 0 ) 
-				{
-					block << "/" 
-					      << stack.top().GetName().GetName() 
-					      << " " << "gs" << endl;
-				}
-
-				if( strcmp( pszToken, "Tf" ) == 0 ) 
-				{
-					size = stack.top().GetReal();
-					stack.pop();
-					block << "/" 
-					      << stack.top().GetName().GetName() 
-					      << " " << size << " " 
-					      << "Tf" << endl;
-				}
-
-				if( strcmp( pszToken, "Tc" ) == 0 ||
-				    strcmp( pszToken, "TL" ) == 0 ||
-				    strcmp( pszToken, "Tw" ) == 0 )
-				{
-					td_y = stack.top().GetReal();
-					stack.pop();
-				
-					block << td_y << " " << pszToken << endl;
-				}
-
-				if( strcmp( pszToken, "TD" ) == 0 ||
-				    strcmp( pszToken, "Td" ) == 0)
-				{
-					td_y = stack.top().GetReal();
-					stack.pop();
-					td_x = stack.top().GetReal();
-					stack.pop();
-				
-					block << td_x << " " 
-					      << td_y << " " 
-					      << pszToken << endl;
-				}
-				else if( strcmp( pszToken, "Tm" ) == 0 )
+			if(record) {
+				if( strcmp( pszToken, "cm" ) == 0 ) 
 				{
 					offset_y = stack.top().GetReal();
 					stack.pop();
@@ -220,45 +81,210 @@ std::string extract( PdfMemDocument* pDocument, PdfPage* pPage )
 					      << shear_y << " " 
 					      << scale_y << " " 
 					      << offset_x << " " 
-					      << offset_y << " " 
-					      << pszToken << endl;
+					      << offset_y << " cm " << endl;
 				}
-				else if( strcmp( pszToken, "Tj" ) == 0 ||
-					 strcmp( pszToken, "'" ) == 0 ) 
+
+				if( strcmp( pszToken, "re" ) == 0 ) 
 				{
-					str = stack.top().GetString().GetString();
-					ReplaceStringInPlace(str, "\\", "\\\\");
-					ReplaceStringInPlace(str, "(", "\\(");
-					ReplaceStringInPlace(str, ")", "\\)");
-		
-					block << "(" << str 
-					      << ")" << pszToken << endl;
+					offset_y = stack.top().GetReal();
 					stack.pop();
+					offset_x = stack.top().GetReal();
+					stack.pop();
+					scale_y  = stack.top().GetReal();
+					stack.pop();
+					shear_y  = stack.top().GetReal();
+					stack.pop();
+				
+					block << shear_y << " " 
+					      << scale_y << " " 
+					      << offset_x << " " 
+					      << offset_y << " " 
+					      << pszToken << " " << endl;
 				}
-				else if( strcmp( pszToken, "TJ" ) == 0 ) 
+
+				if( strcmp( pszToken, "W" ) == 0 ||
+				    strcmp( pszToken, "n" ) == 0 ||
+				    strcmp( pszToken, "q" ) == 0 ||
+				    strcmp( pszToken, "Q" ) == 0 )
 				{
-					PdfArray array = stack.top().GetArray();
+					block << pszToken << endl;
+				}
+
+				if( strcmp( pszToken, "w" ) == 0 ||
+				    strcmp( pszToken, "Tz" ) == 0 ||
+				    strcmp( pszToken, "g" ) == 0 ||
+				    strcmp( pszToken, "G" ) == 0 )
+				{
+					g = stack.top().GetReal();
+					stack.pop();
+					block << g << " " << pszToken << endl;
+				}
+
+				if( strcmp( pszToken, "rg" ) == 0 ||
+				    strcmp( pszToken, "RG" ) == 0 )
+				{
+					blue = stack.top().GetReal();
+					stack.pop();
+					green = stack.top().GetReal();
+					stack.pop();
+					red = stack.top().GetReal();
 					stack.pop();
 
-					block << "[";
-					
-					for( int i=0; i<static_cast<int>(array.GetSize()); i++ ) 
-					{
-						string str;
-						if( array[i].IsString() || array[i].IsHexString() ) {
-							str = array[i].GetString().GetString();
-							ReplaceStringInPlace(str, "\\", "\\\\");
-							ReplaceStringInPlace(str, "(", "\\(");
-							ReplaceStringInPlace(str, ")", "\\)");
-							
-							block << "("
-							      << str
-							      << ")";
-						}
-						else if( array[i].IsNumber())
-							block << array[i].GetReal();
+					blue = 0.0;
+					green = 0.0;
+					red = 0.0;
+				
+					block << red << " " 
+					      << green << " " 
+					      << blue << " " 
+					      << pszToken << endl;
+
+					// cout << red << " " 
+					//       << green << " " 
+					//       << blue << " " 
+					//       << pszToken << endl;
+				}
+
+				// support 'l' and 'm' tokens
+				if( strcmp( pszToken, "l" ) == 0 || 
+				    strcmp( pszToken, "m" ) == 0 )
+				{
+					x = stack.top().GetReal();
+					stack.pop();
+					y = stack.top().GetReal();
+					stack.pop();
+				}
+				else if( strcmp( pszToken, "BT" ) == 0 ) 
+				{
+					bTextBlock   = true;     
+					block << "BT" << endl;
+				}
+				else if( strcmp( pszToken, "ET" ) == 0 ) 
+				{
+					block << "ET" << endl;
+				}
+			}
+
+			if( bTextBlock ) 
+			{
+				if( strcmp( pszToken, "Tf" ) == 0 ) 
+				{
+					size = stack.top().GetReal();
+					stack.pop();
+
+					str = stack.top().GetName().GetName();
+					if(str.compare( removefont ) == 0)
+						record = false;
+					else {
+						if(count)
+							record = true;
+						else
+							record = false;
+						count++;
 					}
-					block << "]" << pszToken << endl;
+
+					if(record)
+						block << "/" 
+						      << str
+						      << " " << size << " " 
+						      << "Tf" << endl;
+
+					// cout << "/" 
+					//       << str
+					//       << " " << size << " " 
+					//       << "Tf" << endl;
+				}
+
+				if(record) {
+					if( strcmp( pszToken, "gs" ) == 0 ) 
+					{
+						block << "/" 
+						      << stack.top().GetName().GetName() 
+						      << " " << "gs" << endl;
+					}
+
+					if( strcmp( pszToken, "Tc" ) == 0 ||
+					    strcmp( pszToken, "TL" ) == 0 ||
+					    strcmp( pszToken, "Tw" ) == 0 )
+					{
+						td_y = stack.top().GetReal();
+						stack.pop();
+				
+						block << td_y << " " << pszToken << endl;
+					}
+
+					if( strcmp( pszToken, "TD" ) == 0 ||
+					    strcmp( pszToken, "Td" ) == 0)
+					{
+						td_y = stack.top().GetReal();
+						stack.pop();
+						td_x = stack.top().GetReal();
+						stack.pop();
+				
+						block << td_x << " " 
+						      << td_y << " " 
+						      << pszToken << endl;
+					}
+					else if( strcmp( pszToken, "Tm" ) == 0 )
+					{
+						offset_y = stack.top().GetReal();
+						stack.pop();
+						offset_x = stack.top().GetReal();
+						stack.pop();
+						scale_y  = stack.top().GetReal();
+						stack.pop();
+						shear_y  = stack.top().GetReal();
+						stack.pop();
+						shear_x  = stack.top().GetReal();
+						stack.pop();
+						scale_x  = stack.top().GetReal();
+						stack.pop();
+				
+						block << scale_x << " " 
+						      << shear_x << " " 
+						      << shear_y << " " 
+						      << scale_y << " " 
+						      << offset_x << " " 
+						      << offset_y << " " 
+						      << pszToken << endl;
+					}
+					else if( strcmp( pszToken, "Tj" ) == 0 ||
+						 strcmp( pszToken, "'" ) == 0 ) 
+					{
+						str = stack.top().GetString().GetString();
+						ReplaceStringInPlace(str, "\\", "\\\\");
+						ReplaceStringInPlace(str, "(", "\\(");
+						ReplaceStringInPlace(str, ")", "\\)");
+		
+						block << "(" << str 
+						      << ")" << pszToken << endl;
+						stack.pop();
+					}
+					else if( strcmp( pszToken, "TJ" ) == 0 ) 
+					{
+						PdfArray array = stack.top().GetArray();
+						stack.pop();
+
+						block << "[";
+					
+						for( int i=0; i<static_cast<int>(array.GetSize()); i++ ) 
+						{
+							string str;
+							if( array[i].IsString() || array[i].IsHexString() ) {
+								str = array[i].GetString().GetString();
+								ReplaceStringInPlace(str, "\\", "\\\\");
+								ReplaceStringInPlace(str, "(", "\\(");
+								ReplaceStringInPlace(str, ")", "\\)");
+							
+								block << "("
+								      << str
+								      << ")";
+							}
+							else if( array[i].IsNumber())
+								block << array[i].GetReal();
+						}
+						block << "]" << pszToken << endl;
+					}
 				}
 
 			}
@@ -358,12 +384,11 @@ void get_glimpse( PdfMemDocument* pDocument, PdfPage* pPage )
 		}
 }
 
-int main()
+void convert(string input,string output, string removefont, int del_start, int del_end)
 {
 	int pn, pc;
 
-	PdfMemDocument pdf("/home/sagar/Downloads/Skandamu10A.pdf");
-	// PdfMemDocument pdf("sk10a.pdf");
+	PdfMemDocument pdf(input.c_str());
 
 	PdfPage*       page = pdf.GetPage(1);
 	PdfObject*     contents = page->GetContents();
@@ -375,6 +400,8 @@ int main()
 	pdf_long       lLen;
 	string         str;
 
+	cout << "Skandam " << input << "Processing" << endl;
+
 	ofstream modified;
 	ofstream actual;
 	modified.open("modified.txt");
@@ -382,7 +409,14 @@ int main()
 
 	PdfError::EnableDebug( false );
 
-	pdf.DeletePages(0, 1);
+	pc = pdf.GetPageCount();
+	if(del_start)
+		pdf.DeletePages(0, del_start);
+
+	if(del_end)
+		pdf.DeletePages(pc - del_end - 1, del_end);
+
+
 	pc = pdf.GetPageCount();
 	for (pn = 0; pn < pc; ++pn) {
 		PdfPage*        page = pdf.GetPage(pn);		
@@ -393,7 +427,7 @@ int main()
 		// actual << pBuffer;
 		// free(pBuffer);
 
-		str = extract(&pdf, page);
+		str = extract(&pdf, page, removefont);
 		// modified << str;
 
 		page->GetContents()->GetStream()->Set(str.c_str());
@@ -406,9 +440,28 @@ int main()
 
 	// page->GetContents()->GetStream()->Set(process_glimps().c_str());
 	
-	pdf.Write("modify.pdf");
-	cout << "Total Glimps: " << size;
-	cout << endl;
+	pdf.Write(output.c_str());
+	// cout << "Total Glimps: " << size;
+	// cout << endl;
+}
 
-	return 0;
+int main()
+{
+	convert("org/Skandamu01.pdf" , "finial/Skandamu01.pdf" , "F1" , 11, 0);
+	convert("org/Skandamu02.pdf" , "finial/Skandamu02.pdf" , "R10", 1, 0);
+	convert("org/Skandamu03.pdf" , "finial/Skandamu03.pdf" , "R10", 1, 0);
+	convert("org/Skandamu04.pdf" , "finial/Skandamu04.pdf" , "F3" , 1, 0);
+	convert("org/Skandamu05.pdf" , "finial/Skandamu05.pdf" , "R10", 1, 4);
+
+	convert("org/Skandamu06.pdf" , "finial/Skandamu06.pdf" , "R10", 1, 0);
+	convert("org/Skandamu07.pdf" , "finial/Skandamu07.pdf" , "R10", 1, 0);
+	convert("org/Skandamu08.pdf" , "finial/Skandamu08.pdf" , "R10", 1, 0);
+	convert("org/Skandamu09.pdf" , "finial/Skandamu09.pdf" , "R10", 1, 4);
+	convert("org/Skandamu10A.pdf", "finial/Skandamu10A.pdf", "F2" , 1, 0);
+
+	convert("org/Skandamu10B.pdf", "finial/Skandamu10B.pdf", "F2" , 1, 0);
+	convert("org/Skandamu10C.pdf", "finial/Skandamu10C.pdf", "F2" , 1, 0);
+	convert("org/Skandamu11A.pdf", "finial/Skandamu11A.pdf", "F3" , 1, 0);
+	convert("org/Skandamu11B.pdf", "finial/Skandamu11B.pdf", "F2" , 1, 0);
+	convert("org/Skandamu12.pdf" , "finial/Skandamu12.pdf" , "F3" , 1, 1);
 }
